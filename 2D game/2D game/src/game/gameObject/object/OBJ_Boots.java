ede37f823dda426c09d91fb38e6ce3d86c66e778
Tran Xuan Huong <phamhyta@gmail.com>
2022-05-23 16:52:24 +0700

    config

--- 2D game/2D game/src/game/gameObject/object/OBJ_Boots.java
+++ 2D game/2D game/src/game/gameObject/object/OBJ_Boots.java
@@ -1,23 +1,21 @@
 package game.gameObject.object;
 
-
 import game.gameObject.Material;
 import game.graphics.SpriteSheet;
 import game.math.Vector2f;
 
 import java.awt.*;
 
-
 public class OBJ_Boots extends Material {
 
-	public OBJ_Boots(Vector2f origin, int size){
+	public OBJ_Boots(Vector2f origin, int size) {
 		super(origin, size);
 
 		name = "Boots";
 		spriteSheet = new SpriteSheet("/objects/boots.png");
-		image =  spriteSheet.getSubimage(0,0,size,size);
+		image = spriteSheet.getSubimage(0, 0, size, size);
 
-		speed =4;
+		speed = 4;
 	}
 
 	@Override
