4bcdc3d44f87fd94bb8f3f66b817b5349412eb58
Tunglt2k2 <tungduonghaphu@gmail.com>
2022-06-16 19:36:14 +0700

    change name of some class. Add ObjectRender class

---
+++ 2D game/src/game/gameObject/object/Potion_Red.java
@@ -0,0 +1,27 @@
+package game.gameObject.object;
+
+import game.gameObject.Player;
+import game.math.Vector2f;
+
+
+public class Potion_Red extends GameObject {
+
+	public Potion_Red(Vector2f origin, int size) {
+		super(origin, size);
+		type= type_consumable;
+		name = "Red Potion";
+		HP =50;
+		objectRender = new ObjectRender(pos, 2, 3,size );
+	}
+
+	public void use(Player player) {
+		if(player.getHealth() + this.HP < player.getMaxHealth()) {
+			player.setCurrentHealth(player.getHealth() + this.HP);
+		}
+		else{
+			player.setCurrentHealth(player.getMaxHealth());
+		}
+	}
+
+
+}
