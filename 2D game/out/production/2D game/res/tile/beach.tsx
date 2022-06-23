76e7f06a0421762339ed2823bc53d01f8f5aac8b
Tran Xuan Huong <phamhyta@gmail.com>
2022-06-23 12:50:08 +0700

    merge

---
+++ 2D game/out/production/2D game/res/tile/beach.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="beach" tilewidth="32" tileheight="32" tilecount="162" columns="18">
+ <image source="beach.png" width="576" height="288"/>
+</tileset>
