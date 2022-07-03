4a6904d8539d8d44bd2457a13fcb27ea80a63f6a
Tunglt2k2 <tungduonghaphu@gmail.com>
2022-07-03 22:08:27 +0700

    push 3/7

--- 2D game/src/game/ai/PathFind.java
+++ 2D game/src/game/ai/PathFind.java
@@ -1,21 +1,39 @@
-//package game.ai;
-//
-//import game.GamePanel;
-//import game.data.GameControl;
-//
-//import java.util.ArrayList;
-//
-//public class PathFind {
-//    Node[][] node;
-//    ArrayList<Node> openList = new ArrayList<>();
-//    public ArrayList<Node> pathList = new ArrayList<>();
-//    Node startNode, goalNode, currentNode;
-//    boolean goalReached= false;
-//    int step =0;
-//    public PathFind(){
-//
-//    }
-//    public void instantiateNodes(){
-//        node = new Node[GameControl.tm.][]
-//    }
-//}
+package game.ai;
+
+import game.GamePanel;
+import game.data.GameControl;
+
+import java.util.ArrayList;
+
+public class PathFind {
+    Node[][] node;
+    ArrayList<Node> openList = new ArrayList<>();
+    public ArrayList<Node> pathList = new ArrayList<>();
+    Node startNode, goalNode, currentNode;
+    boolean goalReached= false;
+    int step =0;
+    int maxCol = GameControl.tm.getWidth();
+    int maxRow = GameControl.tm.getHeight();
+    public PathFind(){
+
+    }
+    public void instantiateNodes(){
+        node = new Node[maxCol][maxRow];
+        int col=0;
+        int row=0;
+        while (col<maxCol && row < maxRow){
+            node[col][row].open = false;
+            node[col][row].checked = false;
+            node[col][row].solid = false;
+
+            col++;
+            if(col == maxCol){
+                col=0;
+                row ++;
+            }
+        }
+    }
+    public void resetNodes(){
+
+    }
+}
