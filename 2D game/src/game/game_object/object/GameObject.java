cdcd2573cba4c8ab44a3696fe5372dfd5a1835fc
Tunglt2k2 <tungduonghaphu@gmail.com>
2022-07-07 20:28:06 +0700

    fix x2

--- 2D game/src/game/game_object/object/GameObject.java
+++ 2D game/src/game/game_object/object/GameObject.java
@@ -71,6 +71,7 @@ public class GameObject {
         if( speed != 0 ){
             player.setMaxSpeed(player.getMaxSpeed() + speed);
         }
+        player.updateHealthManaPercent();
     }
     public void setHP(int hP) {
         HP = hP;
