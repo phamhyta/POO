cce78a95552a1010c0ee5b347daa8b71d5640da3
Tran Xuan Huong <phamhyta@gmail.com>
2022-07-20 23:32:47 +0700

    hi vong la lan cuoi

--- 2D game/game/states/GameIntruction.java
+++ 2D game/game/states/GameIntruction.java
@@ -1,4 +1,5 @@
 package game.states;
+
 import game.GamePanel;
 
 import game.data.GameControl;
@@ -14,7 +15,6 @@ import game.util.MouseHandler;
 import game.math.Vector2f;
 
 import java.awt.*;
-
 
 public class GameIntruction extends GameState {
 
@@ -33,19 +33,20 @@ public class GameIntruction extends GameState {
         GameStateManager.map = new Vector2f(0, 0);
         Vector2f.setWorldVar(GameStateManager.map.x, GameStateManager.map.y);
         this.cam = cam;
-        player = new Player(new Vector2f(0 + (GamePanel.width / 2) , 0 + (GamePanel.height / 2) ), 64);
+        player = new Player(new Vector2f(0 + (GamePanel.width / 2), 0 + (GamePanel.height / 2)), 64);
         playerRender = new EntityRender(player, new SpriteSheet("res/entity/linkFormatted_new.png", 32, 32));
         cam.target(player);
-        gc = new GameControl(player, cam, gsm,true );
+        gc = new GameControl(player, cam, gsm, true);
         pui = new PlayerUI(player);
         npcui = new NpcUI(gc.npc[0]);
-        
+
         setUp();
     }
 
     public void update(double time) {
         Vector2f.setWorldVar(GameStateManager.map.x, GameStateManager.map.y);
-
+        player.setHealth(player.getMaxHealth());
+        player.setCurrentMana(player.getMaxMana());
         playerRender.update();
         player.update(time);
         gc.update(time);
@@ -57,39 +58,42 @@ public class GameIntruction extends GameState {
         mission3();
         mission4();
     }
-    public void setUp(){
-        for(int i = 1; i <= 9; i++){
+
+    public void setUp() {
+        for (int i = 1; i <= 9; i++) {
             this.mission[i] = 0;
         }
     }
+
     public void input(MouseHandler mouse, KeyHandler key) {
         player.input(mouse, key);
         cam.input(mouse, key);
-        if(this.missionDone){
+        if (this.missionDone) {
             gc.npc[0].input(mouse, key);
         }
-        
+
     }
 
-    public void mission1(){
-        if(gc.npc[0].getCurrentConversation()==1&&this.mission[1]==0){
+    public void mission1() {
+        if (gc.npc[0].getCurrentConversation() == 1 && this.mission[1] == 0) {
             this.missionDone = false;
             this.mission[1] = 1;
         }
-        if(gc.npc[0].getCurrentConversation()==1&&this.mission[1]==1){
+        if (gc.npc[0].getCurrentConversation() == 1 && this.mission[1] == 1) {
             gc.npc[0].setTalk(false);
         }
-        if (player.getHitBounds().collides(gc.npc[2].getBounds())&&mission[1]==1){
+        if (player.getHitBounds().collides(gc.npc[2].getBounds()) && mission[1] == 1) {
             this.mission[1] = 2;
             this.mission[2] = 0;
             gc.npc[0].setTalk(true);
             this.missionDone = true;
         }
     }
-    public void mission2(){
-        if(mission[1]==2){
-            if(mission[2]==0){
-                if(gc.npc[0].getCurrentConversation()==4){
+
+    public void mission2() {
+        if (mission[1] == 2) {
+            if (mission[2] == 0) {
+                if (gc.npc[0].getCurrentConversation() == 4) {
                     this.missionDone = false;
                     gc.npc[0].setTalk(false);
                     mission[2] = 1;
@@ -97,8 +101,8 @@ public class GameIntruction extends GameState {
                     gc.origin[1] = pos;
                 }
             }
-            if(mission[2] == 1){
-                if(GameControl.enemy[1]==null){
+            if (mission[2] == 1) {
+                if (GameControl.enemy[1] == null) {
                     mission[2] = 2;
                     gc.npc[0].setTalk(true);
                     this.missionDone = true;
@@ -106,10 +110,11 @@ public class GameIntruction extends GameState {
             }
         }
     }
-    public void mission3(){
-        if(mission[2]==2){
-            if(mission[3]==0){
-                if(gc.npc[0].getCurrentConversation()==6){
+
+    public void mission3() {
+        if (mission[2] == 2) {
+            if (mission[3] == 0) {
+                if (gc.npc[0].getCurrentConversation() == 6) {
                     this.missionDone = false;
                     gc.npc[0].setTalk(false);
                     mission[3] = 1;
@@ -117,8 +122,8 @@ public class GameIntruction extends GameState {
                     gc.origin[3] = pos;
                 }
             }
-            if(mission[3] == 1){
-                if(GameControl.enemy[3]==null){
+            if (mission[3] == 1) {
+                if (GameControl.enemy[3] == null) {
                     mission[3] = 2;
                     gc.npc[0].setTalk(true);
                     this.missionDone = true;
@@ -126,31 +131,31 @@ public class GameIntruction extends GameState {
             }
         }
     }
-    public void mission4(){
-        if(mission[3]==2){
-            if(mission[4]==0){
-                if(gc.npc[0].getCurrentConversation()==8){
+
+    public void mission4() {
+        if (mission[3] == 2) {
+            if (mission[4] == 0) {
+                if (gc.npc[0].getCurrentConversation() == 8) {
                     this.missionDone = false;
                     gc.npc[0].setTalk(false);
                     mission[4] = 1;
                 }
             }
-            if(mission[4] == 1){
+            if (mission[4] == 1) {
                 GameControl.gameObject.add(new Door(new Vector2f(1000, 600), 48));
             }
         }
     }
 
     public void render(Graphics2D g) {
-        
+
         gc.render(g);
         playerRender.render(g);
         pui.render(g);
-        if(gc.npc[0]!=null&&gc.npc[0].isTalking()){
+        if (gc.npc[0] != null && gc.npc[0].isTalking()) {
             npcui.render(g);
         }
-        
-    }
 
+    }
 
 }
